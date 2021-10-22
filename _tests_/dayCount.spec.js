16580e8c2c9c291b1aca369142a6cdb689615439
Jon Tran <trungt8998@gmail.com>
2021-10-22 10:46:11 -0500

    Add Jest Tesing

---
+++ _tests_/dayCount.spec.js
@@ -0,0 +1,7 @@
+import { dayCount} from '../src/client/js/dayCount';
+
+describe("Testing data render function", () => {
+  test("the function should be ", () => {
+    expect(dayCount).toBeDefined()
+  } )
+})
